fd2d362f9a13ea2a68f0321c0e819fbebdc0368b
Snowden Msumba <bis23-smsumba@mubas.ac.mw>
2026-06-28 02:31:43 +0200

    styled the reset password page

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import Register from "./pages/Register";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/register" element={<Register />} />
+        <Route path="/reset/password" element={<ResetPassword />} />
         <Route path="/" element={<Login />} />
         <Route path="/student/dashboard" element={<StudentDashboard />} />
         <Route path="/lecturer/dashboard" element={<LecturerDashboard />} />
--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { resetPassword } from "../services/api";
 import { Eye, EyeOff } from "lucide-react";
+import backGroundImg from "../assets/images/attendance.jpg";
 
 export default function ResetPassword() {
   const [newPassword, setNewPassword] = useState("");
@@ -24,7 +25,7 @@ export default function ResetPassword() {
       await resetPassword({ newPassword, token });
       setSuccess(true);
       setTimeout(() => {
-        navigate("/");
+        navigate(`http://localhost:5173/reset/password?token=${token}`);
       }, 2000);
     } catch (err) {
       setError(
@@ -36,40 +37,68 @@ export default function ResetPassword() {
   };
 
   return (
-    <div>
-      <h2>Make a New password that you will remember</h2>
-      {success && (
-        <div>
-          Password changes successifully. Make sure you dont share with anyone
+    <div
+      className="h-screen bg-cover bg-center w-full rounded-lg flex justify-center items-center p-4"
+      style={{ backgroundImage: `url(${backGroundImg})` }}
+    >
+      <div className="w-full max-w-md bg-gray-500 rounded-lg flex flex-col gap-2 items-center p-8 shadow-md ">
+        <h2 className="text-2xl font-bold text-center text-gray-100">
+          Make a New password that you will remember
+        </h2>
+        {success && (
+          <div className="bg-green-50 text-green-700 p-3 rounded-lg text-sm text-center mb-4">
+            Password changes successifully. Make sure you dont share with anyone
+          </div>
+        )}
+        {error && (
+          <div className="bg-red-50 text-red-600 p-3 rounded-lg text-sm text-center mb-4">
+            {error}
+          </div>
+        )}
+        <div className="w-full relative mb-1">
+          <input
+            className="w-full pl-4 pr-12 py-3 border border-gray-300 rounded-lg text-sm text-white placeholder:text-gray-400 focus:outline-none focus:border-gray-900 bg-transparent"
+            type={showPassword ? "text" : "password"}
+            onChange={(e) => setNewPassword(e.target.value)}
+            value={newPassword}
+            placeholder="New Password"
+          />
+          <button
+            className="absolute inset-y-0 right-3 flex items-center text-gray-300 hover:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <Eye size={20}></Eye> : <EyeOff size={20}></EyeOff>}
+          </button>
         </div>
-      )}
-      <div>
-        {error && <div>{error}</div>}
-        <input
-          type={showPassword ? "text" : "password"}
-          onChange={(e) => setNewPassword(e.target.value)}
-          value={newPassword}
-          placeholder="New Password"
-        />
-        <button onClick={() => setShowPassword(!showPassword)}>
-          {showPassword ? <Eye size={20}></Eye> : <EyeOff size={20}></EyeOff>}
-        </button>
-      </div>
 
-      <div>
-        <input
-          type={showPassword ? "text" : "password"}
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          placeholder="Retype New Password"
-        />
-        <button onClick={() => setShowPassword(!showPassword)}>
-          {showPassword ? <Eye size={20}></Eye> : <EyeOff size={20}></EyeOff>}
+        <div className="w-full relative mb-1">
+          <input
+            className="w-full pl-4 pr-12 py-3 border border-gray-300 rounded-lg text-sm text-white placeholder:text-gray-400 focus:outline-none focus:border-gray-900 bg-transparent"
+            type={showPassword ? "text" : "password"}
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Retype New Password"
+          />
+          <button
+            className="absolute inset-y-0 right-3 flex items-center text-gray-300 hover:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
+            onClick={() => setShowPassword(!showPassword)}
+          >
+            {showPassword ? <Eye size={20}></Eye> : <EyeOff size={20}></EyeOff>}
+          </button>
+        </div>
+        <button
+          className="p-3 bg-green-500 text-white rounded-lg hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 transition-all disabled:opacity-50"
+          type="button"
+          disabled={loading}
+          onClick={() => {
+            handleChangePassword;
+            setNewPassword("");
+            setConfirmPassword("");
+          }}
+        >
+          {loading ? "Changing..." : "Change Password"}
         </button>
       </div>
-      <button type="button" disabled={loading} onClick={handleChangePassword}>
-        {loading ? "Changing..." : "Change Password"}
-      </button>
     </div>
   );
 }
