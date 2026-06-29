f50a35a85a1fef928e9cd85097d5cab10cb5f217
Snowden Msumba <bis23-smsumba@mubas.ac.mw>
2026-06-29 23:55:05 +0200

    added a hamburger button for menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
 import { useState, useRef } from "react";
+import { Menu, X } from "lucide-react";
 
 function Navbar() {
   const { user, logout } = useAuth();
@@ -8,12 +9,12 @@ function Navbar() {
   const [imageUrl, setImageUrl] = useState(
     localStorage.getItem(`profileImage_${user?.email}`) || null,
   );
+  const [openMenu, setOpenMenu] = useState(false);
   const fileInputRef = useRef(null);
 
   const handleClickButton = () => {
     fileInputRef.current.click();
   };
-
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
@@ -35,46 +36,78 @@ function Navbar() {
   };
 
   return (
-    <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center sticky top-0 z-10">
-      <div className="flex items-center gap-3">
-        <input
-          className="hidden"
-          type="file"
-          ref={fileInputRef}
-          onChange={handleFileChange}
-          accept="image/*"
-        />
-        <button
-          className="bg-transparent border-none cursor-pointer p-0 rounded-full overflow-hidden w-10 h-10 shadow-md flex-shrink-0"
-          onClick={handleClickButton}
-        >
-          {imageUrl ? (
-            <img
-              src={imageUrl}
-              alt="profile"
-              className="w-full h-full object-cover rounded-full"
-            />
-          ) : (
-            <div className="w-10 h-10 rounded-full bg-gray-600 flex items-center justify-center text-white font-bold text-sm">
-              {user?.first_name?.[0] || "U"}
-            </div>
-          )}
-        </button>
-        <h1 className="text-lg font-bold">Attendance System</h1>
-      </div>
+    <div className="relative">
+      <nav className="bg-gray-900 text-white px-6 py-4 flex justify-between items-center sticky top-0 z-10">
+        <div className="flex items-center gap-3">
+          <input
+            className="hidden"
+            type="file"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+            accept="image/*"
+          />
+          <button
+            className="bg-transparent border-none cursor-pointer p-0 rounded-full overflow-hidden w-10 h-10 shadow-md flex-shrink-0"
+            onClick={handleClickButton}
+          >
+            {imageUrl ? (
+              <img
+                src={imageUrl}
+                alt="profile"
+                className="w-full h-full object-cover rounded-full"
+              />
+            ) : (
+              <div className="w-10 h-10 rounded-full bg-gray-600 flex items-center justify-center text-white font-bold text-sm">
+                {user?.first_name?.[0] || "U"}
+              </div>
+            )}
+          </button>
+          <h1 className="text-lg font-bold">Attendance System</h1>
+        </div>
 
-      <div className="flex items-center gap-4">
-        <span className="text-gray-400 text-sm">
-          {user?.first_name} {user?.last_name}
-        </span>
+        <div className="hidden md:flex items-center gap-4">
+          <span className="text-white text-sm">
+            {user?.first_name} {user?.last_name}
+          </span>
+          <button
+            className="px-4 py-1.5 border border-white rounded-lg text-sm hover:bg-white hover:text-gray-900 transition-all"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
         <button
-          className="px-4 py-1.5 border border-white rounded-lg text-sm hover:bg-white hover:text-gray-900 transition-all"
-          onClick={handleLogout}
+          className="flex md:hidden"
+          onClick={() => {
+            setOpenMenu(!openMenu);
+          }}
         >
-          Logout
+          <Menu size={20}></Menu>
         </button>
-      </div>
-    </nav>
+      </nav>
+      {openMenu && (
+        <div className="absolute top-2  right-0 w-1/2 bg-gray-900 rounded-sm flex md:hidden flex-col gap-2 p-4 z-10">
+          <button
+            className="flex md:hidden right-0 self-end mb-2 text-white px-4 py-1.5 border-white rounded-lg text-sm hover:bg-white hover:text-gray-900 transition-all"
+            onClick={() => {
+              setOpenMenu(!openMenu);
+            }}
+          >
+            <X size={20}></X>
+          </button>
+
+          <span className="text-white text-sm">
+            {user?.first_name} {user?.last_name}
+          </span>
+          <button
+            className="text-white px-4 py-1.5 border border-white rounded-lg text-sm hover:bg-white transition-all"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
   );
 }
 
