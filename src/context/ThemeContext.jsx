6cfdd4a45195690bb2e0b9747e7a266d221cd78d
Snowden Msumba <bis23-smsumba@mubas.ac.mw>
2026-07-03 20:23:38 +0200

    created a the context

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,1 +1,18 @@
-0882342832;
+import { createContext, useState, useContext } from "react";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({ children }) {
+  const [mode, setMode] = useState(false);
+
+  const changeMode = () => {
+    setMode(!mode);
+  };
+  return (
+    <ThemeContext.Provider value={{ mode, changeMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => useContext(ThemeContext);
