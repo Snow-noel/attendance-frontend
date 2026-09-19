cc93e8f45c8d58132d269b730e77805684aaa8a7
Snowden Msumba <bis23-smsumba@mubas.ac.mw>
2026-09-19 10:57:21 +0200

    added the authentication details for lecturer

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,28 +3,51 @@ import { createContext, useState, useContext } from "react";
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-  const [user, setUser] = useState(null);
+  const [studentUser, setStudentUser] = useState(null);
+  const [studentToken, setStudentToken] = useState(
+    localStorage.getItem("studentToken") || null,
+  );
 
-  const [token, setToken] = useState(localStorage.getItem("token") || null);
+  const [lecturerUser, setLecturerUser] = useState(null);
+  const [lecturerToken, setLecturerToken] = useState(
+    localStorage.getItem("lecturerToken") || null,
+  );
 
   const login = (userData, userToken) => {
-    setUser(userData);
-
-    setToken(userToken);
-
-    localStorage.setItem("token", userToken);
+    if (userData.role === "student") {
+      setStudentUser(userData);
+      setStudentToken(userToken);
+      localStorage.setItem("studentToken", userToken);
+    } else if (userData.role === "lecturer") {
+      setLecturerUser(userData);
+      setLecturerToken(userToken);
+      localStorage.setItem("lecturerToken", userToken);
+    }
   };
 
-  const logout = () => {
-    setUser(null);
-
-    setToken(null);
-
-    localStorage.removeItem("token");
+  const logout = (role) => {
+    if (role === "student") {
+      setStudentUser(null);
+      setStudentToken(null);
+      localStorage.removeItem("studentToken");
+    } else if (role === "lecturer") {
+      setLecturerUser(null);
+      setLecturerToken(null);
+      localStorage.removeItem("lecturerToken");
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ user, token, login, logout }}>
+    <AuthContext.Provider
+      value={{
+        studentUser,
+        studentToken,
+        lecturerUser,
+        lecturerToken,
+        login,
+        logout,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
