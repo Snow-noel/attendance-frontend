44032a69568f1d2a9539f6d155f17d809d63b45c
Snowden Msumba <bis23-smsumba@mubas.ac.mw>
2026-09-19 10:57:48 +0200

    added protection to routes

--- src/components/RouteProtector.jsx
+++ src/components/RouteProtector.jsx
@@ -2,8 +2,10 @@ import { useAuth } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 export default function RouterProtector({ children, role }) {
-  const { token, user } = useAuth();
+  const { studentToken, studentUser, lecturerToken, lecturerUser } = useAuth();
 
+  const token = role === "student" ? studentToken : lecturerToken;
+  const user = role === "student" ? studentUser : lecturerUser;
   if (!token) {
     return <Navigate to="/" />;
   }
